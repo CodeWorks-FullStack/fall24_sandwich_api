addc9684c8122518b316b4bca2730c4fe6e6fe19
Jeremy Fowler <jembyfowler@gmail.com>
2024-10-15 11:53:06 -0600

    get test

--- Controllers/SandwichesController.cs
+++ Controllers/SandwichesController.cs
@@ -5,9 +5,24 @@ namespace sandwich_api.Controllers;
 [Route("api/sandwiches")] // super('api/sandwiches')
 public class SandwichesController : ControllerBase // extends BaseController
 {
-  [HttpGet]
+  [HttpGet("test")] // .get('/test', this.testGet)
   public string TestGet()
   {
     return "API is working!";
+  }
+
+  // ActionResult type is an HTTP response type
+  [HttpGet]
+  public ActionResult<List<Sandwich>> GetSandwiches()
+  {
+    try
+    {
+      List<Sandwich> sandwiches = [];
+      return Ok(sandwiches);
+    }
+    catch (System.Exception error)
+    {
+      return BadRequest(error.Message);
+    }
   }
 }
