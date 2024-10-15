550c8258ade978c305a51f6cd02ac730a4b43248
Jeremy Fowler <jembyfowler@gmail.com>
2024-10-15 12:28:08 -0600

    repo retrieving data from database

--- Repositories/SandwichesRepository.cs
+++ Repositories/SandwichesRepository.cs
@@ -4,8 +4,19 @@ namespace sandwich_api.Repositories;
 // NOTE all database logic should take place in the repository level
 public class SandwichesRepository
 {
+  public SandwichesRepository(IDbConnection db)
+  {
+    _db = db;
+  }
+  private readonly IDbConnection _db;
+
+
   internal List<Sandwich> GetSandwiches()
   {
-    return [];
+    string sql = "SELECT * FROM sandwiches;";
+
+    // dbContext.Cars.find()
+    List<Sandwich> sandwiches = _db.Query<Sandwich>(sql).ToList();
+    return sandwiches;
   }
 }
